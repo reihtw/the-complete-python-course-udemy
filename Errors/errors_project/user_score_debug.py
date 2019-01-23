a50fc11c26a7cc6a465761c3603cb5bdfe994865
Dusan Nemec <38624839+dusannemec@users.noreply.github.com>
2019-01-23 18:29:42 -0200

    Ending errors section

--- Errors/errors_project/user_score_debug.py
+++ Errors/errors_project/user_score_debug.py
@@ -3,9 +3,10 @@ class User:
         self.name = name
         self.engagement_metrics = engagement
         self.score = 0
-    
+
     def __repr__(self):
         return f'<User {self.name}>'
+
 
 def email_engaged_user(user):
     try:
@@ -16,11 +17,14 @@ def email_engaged_user(user):
         if user.score > 500:
             send_engagement_notification(user)
 
+
 def perform_calculation(metrics):
     return metrics['clicks'] * 5 + metrics['hits'] * 2
 
+
 def send_engagement_notification(user):
     print(f'Notification sent to {user}')
+
 
 my_user = User('Rolf', {'clicks': 61, 'hits': 100})
 email_engaged_user(my_user)
