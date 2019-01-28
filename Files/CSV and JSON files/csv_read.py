c7a6000b59ddb98f341c3b9c1cc7eda9ec8262f0
Dusan Nemec <38624839+dusannemec@users.noreply.github.com>
2019-01-28 11:43:46 -0200

    CSV files in Python

--- Files/CSV and JSON files/csv_read.py
+++ Files/CSV and JSON files/csv_read.py
@@ -1,4 +1,12 @@
 csv_file = open('csv_data.txt', 'r')
-csv = [line.strip() for line in csv_file.readlines()]
-
+csv = [line.strip() for line in csv_file.readlines()[1:]]
 csv_file.close()
+
+for line in csv:
+    person_data = line.split(',')
+    name = person_data[0].title()
+    age = person_data[1]
+    university = person_data[2].title()
+    degree = person_data[3].capitalize()
+
+    print(f'{name} is {age}, studying {degree} at {university}.')
