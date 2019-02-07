278b0d542480ac79c74f41207eb4ad8f8c08411c
Dusan Nemec <38624839+dusannemec@users.noreply.github.com>
2019-02-06 22:45:18 -0200

    Updating

--- Milestone Project 2/DB WITH/utils/database.py
+++ Milestone Project 2/DB WITH/utils/database.py
@@ -1,4 +1,4 @@
-import sqlite3
+from .database_connection import DatabaseConnection
 
 """
 Concerned with storing and retrieving books from a list.
@@ -6,50 +6,29 @@ Concerned with storing and retrieving books from a list.
 
 
 def create_book_table():
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
+        with DatabaseConnection('data.db') as cursor:
+                cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)')
 
-        cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)')
-
-        connection.commit()
-        connection.close()
 
 def add_book(name, author):
         # *,0); DROP TABLE books; -> SQL Injection Vulnerability if change strings directly
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        cursor.execute('INSERT INTO books VALUES(?, ?, 0)', (name, author))
-
-        connection.commit()
-        connection.close()
+        with DatabaseConnection('data.db') as cursor:
+                cursor.execute('INSERT INTO books VALUES(?, ?, 0)', (name, author))
 
 
 def list_book():
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
+        with DatabaseConnection('data.db') as cursor:
+                cursor.execute('SELECT * FROM books')
+                books = [{'name': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()] # [(name, author, read), (name, author, read)]
 
-        cursor.execute('SELECT * FROM books')
-        books = [{'name': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()] # [(name, author, read), (name, author, read)]
-
-        connection.close()
         return books      
 
 
 def read_book(name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
+        with DatabaseConnection('data.db') as cursor:
+                cursor.execute('UPDATE books SET read=1 WHERE name=?', (name,))
 
-        cursor.execute('UPDATE books SET read=1 WHERE name=?', (name,))
-
-        connection.commit()
-        connection.close()
 
 def del_book(name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        cursor.execute('DELETE FROM books WHERE name=?', (name,))
-
-        connection.commit()
-        connection.close()
+        with DatabaseConnection('data.db') as cursor:
+                cursor.execute('DELETE FROM books WHERE name=?', (name,))
