32beb006f179f6c6807fbf259660ebfc9309b048
Dusan Nemec <38624839+dusannemec@users.noreply.github.com>
2019-02-15 21:09:19 -0200

    section 11

---
+++ Web Scrapping/Understanding HTML/simple_html.py
@@ -0,0 +1,14 @@
+SIMPLE_HTML = '''<html>
+<head></head>
+<body>
+<h1>This is a title</h1>
+<p class="subtitle">Lorem ipsum dolor sit amet. Consectetur edipiscim elit.</p>
+<p>Here's another p without a class</p>
+<ul>
+	<li>Rolf</li>
+	<li>Charlie</li>
+	<li>Jen</li>
+	<li>Jose</li>
+</ul>
+</body>
+</html>'''
