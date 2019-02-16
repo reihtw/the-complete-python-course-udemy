aa7b7ffa6b86f536944f8a020299a63fad941a85
Dusan Nemec <38624839+dusannemec@users.noreply.github.com>
2019-02-16 15:46:22 -0200

    section 11 web scrapping

--- Web Scrapping/Understanding HTML/simple_html.py
+++ Web Scrapping/Understanding HTML/simple_html.py
@@ -1,3 +1,5 @@
+from bs4 import BeautifulSoup
+
 SIMPLE_HTML = '''<html>
 <head></head>
 <body>
@@ -12,3 +14,32 @@ SIMPLE_HTML = '''<html>
 </ul>
 </body>
 </html>'''
+
+simple_soup = BeautifulSoup(SIMPLE_HTML, 'html.parser')
+
+def find_title():
+	h1_tag = simple_soup.find('h1')
+	print(h1_tag.string)
+
+
+def find_list_items():
+	list_items = simple_soup.find_all('li')
+	list_contents = [l.string for l in list_items]
+	print(list_contents)
+
+
+def find_subtitle():
+	paragraph = simple_soup.find('p', {'class': 'subtitle'})
+	print(paragraph.string)
+
+
+def find_other_paragraph():
+	paragraphs = simple_soup.find_all('p')
+	other_paragraph = [p for p in paragraphs if 'subtitle' not in p.attrs.get('class', [])]
+	print(other_paragraph[0].string)
+
+
+find_title()
+find_list_items()
+find_subtitle()
+find_other_paragraph()
