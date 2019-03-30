6bc9d03529e1afd05025b349a3a58a5b51c70151
Dusan Nemec <38624839+dusannemec@users.noreply.github.com>
2019-03-30 19:20:26 -0300

    Web Scrapping ended

--- Web Scrapping/Milestone Project 4 (correction)/scraping-books/app.py
+++ Web Scrapping/Milestone Project 4 (correction)/scraping-books/app.py
@@ -1,11 +1,22 @@
 import requests
+import logging
 
 from pages.all_books_page import AllBooksPage
 
-page_content = requests.get('http://books.toscrape.com').content
+logging.basicConfig(format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+                    datefmt='%d-%m-%Y %H:%M:%S',
+                    level=logging.DEBUG,
+                    filename='logs.txt')
+
+logger = logging.getLogger('scraping')
+
+logger.info('Loading books list...')
+
+books = []
+page_content = requests.get(f'http://books.toscrape.com/').content
 page = AllBooksPage(page_content)
 
-books = page.books
-
-for book in books:
-    print(book)
+for i in range(page.page_count):
+    page_content = requests.get(f'http://books.toscrape.com/catalogue/page-{i+1}.html').content
+    page = AllBooksPage(page_content)
+    books.extend(page.books)
