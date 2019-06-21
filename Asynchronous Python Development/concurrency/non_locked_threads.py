9ae617a9b7b9c21a2febf9e848eedee75775f80f
Rodrigo Lins <rodlins@icloud.com>
2019-06-21 17:34:45 -0300

    iMac

--- Asynchronous Python Development/concurrency/non_locked_threads.py
+++ Asynchronous Python Development/concurrency/non_locked_threads.py
@@ -6,16 +6,18 @@ from threading import Thread
 counter = 0
 
 def increment_counter():
+    time.sleep(random.random())
     global counter
     time.sleep(random.random())
     counter += 1
     time.sleep(random.random())
     print(f'New counter value: {counter}')
     time.sleep(random.random())
-    print('------------')
-
+    print('-------------------------')
+    time.sleep(random.random())
 
 for x in range(10):
+    time.sleep(random.random())
     t = Thread(target=increment_counter)
     time.sleep(random.random())
     t.start()
--- Asynchronous Python Development/concurrency/queued_threads.py
+++ Asynchronous Python Development/concurrency/queued_threads.py
@@ -5,20 +5,19 @@ import queue
 from threading import Thread
 
 counter = 0
-job_queue = queue.Queue() # things to printed out
-counter_queue = queue.Queue() # amounts by which to increase counter
+job_queue = queue.Queue() # things to be printed out
+counter_queue = queue.Queue() # amount by to which increase counter
 
 def increment_manager():
     global counter
     while True:
-        increment = counter_queue.get() #this waits until an item is available and then locks
+        increment = counter_queue.get() # this waits until an item is available and then locks the queue
         old_counter = counter
         counter = old_counter + increment
-        job_queue.put(f'New counter value is {counter}', '==========')
-        counter_queue.task_done()
+        job_queue.put((f'New counter value is {counter}', '-----------'))
+        counter_queue.task_done() # this unlocks the queue
 
 Thread(target=increment_manager, daemon=True).start()
-
 
 def printer_manager():
     while True:
@@ -27,7 +26,6 @@ def printer_manager():
         job_queue.task_done()
 
 Thread(target=printer_manager, daemon=True).start()
-
 
 def increment_counter():
     counter_queue.put(1)
